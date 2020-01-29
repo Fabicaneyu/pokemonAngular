ea073d68d9510ecede5cc07b7ed1213dab50fab7
Fabí Canedo Yugar <fabicaneyu@gmail.com>
2020-01-29 17:07:22 -0300

    criando componentes

--- pokemonAPP/src/app/app.module.ts
+++ pokemonAPP/src/app/app.module.ts
@@ -1,12 +1,17 @@
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponent,
+    MeuPrimeiro2Component
   ],
   imports: [
     BrowserModule,
